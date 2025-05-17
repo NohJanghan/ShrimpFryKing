c79d2d2e0a5b0d2b8da1d320a9fa9dce835629cc
Minkyung Kim <kimminkyung@gm.gist.ac.kr>
2025-05-18 00:56:13 +0900

    get-comment-summary

--- backend/routers/comment_router.py
+++ backend/routers/comment_router.py
@@ -1,14 +1,9 @@
 from fastapi import APIRouter
 from routers.dto import comment_dto
-from services.comment_service.get_comments import get_comments
 from services.comment_service.post_comment import post_comment
 from services.comment_service.get_comment_summary import get_comment_summary
 
 router = APIRouter(prefix="/comment", tags=["comment"])
-
-@router.get('/', response_model=list[comment_dto.CommentItem])
-async def get_comments_handler(news_id: int):
-    return await get_comments(news_id)
 
 @router.post('/', response_model=bool)
 async def post_comment_handler(news_id: int, comment: comment_dto.PostCommentRequest):
