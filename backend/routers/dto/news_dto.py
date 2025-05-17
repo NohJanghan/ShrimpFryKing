36959da5fab19c5e02db970a78536d2f2a609ee1
Janghan Noh <felkared@gmail.com>
2025-05-18 00:44:30 +0900

    news item detail is same with news item

--- backend/routers/dto/news_dto.py
+++ backend/routers/dto/news_dto.py
@@ -9,12 +9,18 @@ class NewsItemBrief(BaseModel):
     dislikes: int
 
 class NewsItemDetail(BaseModel):
-    id: int
+    news_id: int
     title: str
-    brief: str
     content: str
-    imageURL: str
-    likes: int
-    dislikes: int
-    # comments
-
+    url: str
+    image_url: str
+    category: str
+    brief: str
+    author_id: str
+    date: int
+    like: int
+    dislike: int
+    opinion: int
+    comment: list['CommentItem'] # list of CommentItem
+    Isliked: bool
+    Isdisliked: bool
