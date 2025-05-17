5be517af10c15b8b2f65a3d4393fd9076338119c
Minkyung Kim <kimminkyung@gm.gist.ac.kr>
2025-05-17 23:55:48 +0900

    news-service

---
+++ backend/services/news_service/create_news.py
@@ -0,0 +1,41 @@
+from fastapi import HTTPException
+from routers.dto import news_dto
+from crawling.article_extractor import extract_articles_from_url
+from categorizer.article_classifier import classify_article
+from summarizer.gemini_summary import summarize_article
+from db.DBservice import db
+from db.entity import CreateNewsItem
+from utils.article import get_first_image_url
+
+
+async def create_news(news_url: str):
+    try:
+        article = extract_articles_from_url(news_url)
+        print(f"[INFO] Extracted article - article: {article}")
+        content = article["content"]
+        title = article["title"]
+        if not article or len(content.strip()) == 0:
+            raise HTTPException(status_code=400, detail="Failed to extract article content from the given URL.")
+
+        category = classify_article(content)
+        image_url = get_first_image_url(content)
+        brief = summarize_article(content)
+
+        print(f"[INFO] Analysis complete - Category: {category}")
+        # Insert in DB
+        data = CreateNewsItem(
+            title=title,
+            content=content,
+            url=news_url,
+            image_url=image_url,
+            category=category,
+            brief=brief,
+            author_id="0",  # Placeholder for author ID
+        )
+        # print(f"[INFO] Inserting news into DB - Title: {title}, URL: {news_url}")
+        db.create_news(data)
+        return True
+
+    except Exception as e:
+        print(f"[ERROR] Failed to process news: {e}")
+        raise HTTPException(status_code=500, detail="An error occurred while processing the news article.")
---
+++ backend/services/news_service/get_news.py
@@ -0,0 +1,24 @@
+from fastapi import HTTPException
+from routers.dto import news_dto
+from typing import Literal
+from crawling.article_extractor import extract_articles_from_url
+from categorizer.article_classifier import classify_article
+from summarizer.gemini_summary import summarize_article
+from db.DBservice import db
+from db.entity import CreateNewsItem
+from utils.article import get_first_image_url
+
+
+async def get_news(
+        order_by: Literal['recent', 'hot'],
+        page: int = 1,
+        page_size: int = 10
+):
+    try:
+        news_items = db.get_news_list(order_by, page, page_size)
+
+        return [news_dto.NewsItemBrief.from_orm(item) for item in news_items]
+    
+    except Exception as e:
+        print(f"[Error] Failed to get news list: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
