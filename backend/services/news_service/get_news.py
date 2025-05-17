aba9688dc695a8f440d285a8a3443ad421a48001
Janghan Noh <felkared@gmail.com>
2025-05-18 00:21:34 +0900

    fix get_news.py

--- backend/services/news_service/get_news.py
+++ backend/services/news_service/get_news.py
@@ -1,4 +1,5 @@
 from fastapi import HTTPException
+from backend.utils.convert_type import news_to_detail
 from routers.dto import news_dto
 from typing import Literal
 from crawling.article_extractor import extract_articles_from_url
@@ -16,9 +17,10 @@ async def get_news(
 ):
     try:
         news_items = db.get_news_list(order_by, page, page_size)
+        news_items = list(map(lambda item: news_to_detail(item), news_items))
 
-        return [news_dto.NewsItemBrief.from_orm(item) for item in news_items]
-    
+        return news_items
+
     except Exception as e:
         print(f"[Error] Failed to get news list: {e}")
         raise HTTPException(status_code=500, detail=str(e))
