66598086af28ef2e7df9d0a173adca6a1f99b753
Janghan Noh <felkared@gmail.com>
2025-05-18 04:20:35 +0900

    Add user service

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 from dotenv import load_dotenv
+
 load_dotenv()
 from fastapi import FastAPI
-from routers import news_router, comment_router
+from routers import news_router, comment_router, user_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Replace with your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
@@ -11,3 +20,4 @@ async def root():
 
 app.include_router(news_router.router)
 app.include_router(comment_router.router)
+app.include_router(user_router.router)
