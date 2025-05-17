93bce33c43c480ed7798625884025c5261ca9339
Janghan Noh <felkared@gmail.com>
2025-05-18 07:01:22 +0900

    Better way

--- backend/crawling/article_extractor.py
+++ backend/crawling/article_extractor.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from markdownify import markdownify as md
+import html2text
 
 def extract_articles_from_url(url):
   try:
@@ -9,24 +10,30 @@ def extract_articles_from_url(url):
     res.raise_for_status()
 
     soup = BeautifulSoup(res.text, 'html.parser')
-    
+
     og_title = soup.find("meta", property="og:title")
     if og_title and og_title.get("content"):
       title = og_title["content"].strip()
     else:
       title_tag = soup.find("title")
       title = title_tag.get_text(strip=True) if title_tag else "No Title Found"
-    
+
     articles = soup.find_all('article')
-    
+
     articles = '\n\n'.join(article.prettify() for article in articles)
     articles = articles.replace("data-src", "src")
 
+    h = html2text.HTML2Text()
+    h.ignore_links = False
+    h.ignore_images = False
+    h.single_line_break = False
+    h.ignore_tables = True
+
     return {
       'title': title,
-      'content': md(articles)
+      'content': h.handle(articles)
       }
-  
+
   except requests.exceptions.RequestException as e:
     print(f"[ERROR] Request failed: {e}")
-    return md('')
+    return ''
