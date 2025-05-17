39afdcdbc65fd8743a4080dc43d565de4f245da6
Minkyung Kim <kimminkyung@gm.gist.ac.kr>
2025-05-17 18:56:10 +0900

    article-classifier

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,17 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from crawling.article_extractor import extract_articles_from_url
 from summarizer.gemini_summary import summarize_article
+from categorizer.article_classifier import classify_article
 
 url = 'https://n.news.naver.com/article/584/0000032393?cds=news_media_pc&type=editn'
 
 articles = extract_articles_from_url(url)
 
+category = classify_article(articles)
 summary = summarize_article(articles)
 
+print(category)
 print(summary)
--- backend/summarizer/gemini_summary.py
+++ backend/summarizer/gemini_summary.py
@@ -2,7 +2,6 @@ import google.generativeai as genai
 import os
 
 api_key = os.getenv("GEMINI_API_KEY")
-
 genai.configure(api_key=api_key)
 
 def summarize_article(text):
