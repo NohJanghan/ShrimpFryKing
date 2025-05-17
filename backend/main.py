97cd0cefce81105b26243fa0ee170c9ba2e06aa4
Janghan Noh <felkared@gmail.com>
2025-05-17 17:10:18 +0900

    make news router

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from routers import news_router
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+app.include_router(news_router.router)
