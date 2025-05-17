f28e876bbd0d9a9d0e36dabc8d1a830b6ba0f19c
Janghan Noh <felkared@gmail.com>
2025-05-18 05:49:17 +0900

    add skeleton of (dis)like

--- backend/routers/news_router.py
+++ backend/routers/news_router.py
@@ -4,6 +4,7 @@ from typing import Literal
 from services.news_service.get_news import get_news
 from services.news_service.get_news_by_id import get_news_by_id
 from services.news_service.create_news import create_news
+from services.news_service.like_news import like_news, dislike_news, remove_like_news, remove_dislike_news
 from services.user_service import session_store
 from routers.middlewares import auth_middleware
 
@@ -31,3 +32,25 @@ async def get_news_by_id_handler(news_id: int, request: Request, response: Respo
 async def create_news_handler(news_url: str, response: Response, request: Request):
     user_id = auth_middleware(request, response)
     return await create_news(news_url, user_id)
+
+@router.post("/like/{news_id}", response_model=bool)
+async def like_news_handler(news_id: int, request: Request, response: Response):
+    user_id = auth_middleware(request, response)
+    return await like_news(news_id, user_id)
+
+
+@router.post("/dislike/{news_id}", response_model=bool)
+async def dislike_news_handler(news_id: int, request: Request, response: Response):
+    user_id = auth_middleware(request, response)
+    return await dislike_news(news_id, user_id)
+
+@router.delete("/like/{news_id}", response_model=bool)
+async def remove_like_news_handler(news_id: int, request: Request, response: Response):
+    user_id = auth_middleware(request, response)
+    return await remove_like_news(news_id, user_id)
+
+
+@router.delete("/dislike/{news_id}", response_model=bool)
+async def remove_dislike_news_handler(news_id: int, request: Request, response: Response):
+    user_id = auth_middleware(request, response)
+    return await remove_dislike_news(news_id, user_id)
