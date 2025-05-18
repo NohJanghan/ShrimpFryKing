55c177cfb18e5fc6afc3cbd454b7851fcff14848
172635 <hjmath04@naver.com>
2025-05-18 09:57:34 +0900

    logout see

--- backend/services/comment_service/post_comment_like.py
+++ backend/services/comment_service/post_comment_like.py
@@ -3,6 +3,9 @@ from db.DBservice import db
 
 async def post_comment_like(news_id, comment_i, user_id):
   try:
+    if user_id is "":
+      # guest
+      False
     news_item = db.get_news_by_id(news_id, user_id=user_id)
     comment_item = news_item.comment[comment_i]
 
@@ -23,6 +26,9 @@ async def post_comment_like(news_id, comment_i, user_id):
 
 async def delete_comment_like(news_id, comment_i, user_id):
   try:
+    if user_id is "":
+      # guest
+      False
     news_item = db.get_news_by_id(news_id, user_id=user_id)
     comment_item = news_item.comment[comment_i]
 
